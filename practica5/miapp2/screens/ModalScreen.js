1af99fe67738afd3ad9575e5aead02fe94963f81
ARC <rioscarballoa@gmail.com>
2026-07-02 16:49:05 -0600

    Practica No. 18: Modal & BottomSheet

--- practica5/miapp2/screens/ModalScreen.js
+++ practica5/miapp2/screens/ModalScreen.js
@@ -1,28 +1,77 @@
 /*Zona 1:   Importaciones de componentes y archivos*/
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Modal, Button, Pressable } from 'react-native';
 
 
-/*Zona 2:  Main - Hogar de los componentes */
-export default function ModalScreen() {
-  return (
+export default function ModalBottomSheet() { 
+  const [modalVisible, setModalVisible] = useState(false);
+  return(
     <View style={styles.container}>
-
-        <Text>Aquí va la practica de Rafael</Text>
-
-      <StatusBar style="auto" />
-    
+        <Text style = {styles.titulo}>Ejemplo de modal y bottom sheet</Text>
+        <Button title="Abrir Modal" onPress={() => setModalVisible(true)} />
+        <Modal 
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+        >
+          <View style={styles.fondo}>
+            <View style={styles.bottomSheet}>
+              <Text style={styles.texto}>
+                Hola esto es un BottomSheet
+              </Text>
+              <Pressable onPress={() => setModalVisible(false)}>
+                <Text style={styles.boton}>
+                  Cerrar
+                </Text> 
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
     </View>
   );
 }
+
 
 /*Zona 3: Estilos y posicionamiento */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  titulo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  fondo: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    backgroundColor: 'rgba(0,0,0,0.4)',
+  },
+  bottomSheet: {
+    backgroundColor: '#fff',
+    padding: 25,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    alignItems: 'center',
+  },
+  texto: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  boton: {
+    backgroundColor: '#2196F3',
+    paddingHorizontal: 25,
+    paddingVertical: 10,
+    borderRadius: 8,
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  textoBoton: {
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
